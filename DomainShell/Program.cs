c302a802c8b4a87bc5f59ae12916f6af3dd1e598
davidrostcheck <davidrostcheck@gmail.com>
2014-12-27 00:03:03 -0300

    Begining sketches of a command shell

---
+++ DomainShell/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainShell
+{
+    /// <summary>
+    /// MercurioShell allows the user to manipulate a Mercurio environment from the command line 
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
