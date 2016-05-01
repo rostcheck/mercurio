92e5708efea3ec7156019b6e57e3e4154cf94dc1
David Rostcheck <davidr@thisisdavidr.net>
2016-05-01 20:51:41 -0300

    * ImportDocumentCommand.cs: Re-enabled commented-out functionality
    * Todo.txt: Modified to reflect fix to import document command

--- MercurioShell/Commands/ImportDocumentCommand.cs
+++ MercurioShell/Commands/ImportDocumentCommand.cs
@@ -22,8 +22,8 @@ namespace MercurioShell
 
             var documentPath = arguments["document-path"];
             var documentName = Path.GetFileName(documentPath);
-            //var documentData = File.ReadAllText(documentPath);
-            //var documentVersion = context.OpenContainer.CreateTextDocument(documentName, context.Environment.GetActiveIdentity(), documentData);
+            var documentData = File.ReadAllText(documentPath);
+            var documentVersion = context.OpenContainer.CreateTextDocument(documentName, context.Environment.GetActiveIdentity(), documentData);
             var returnList = new List<string>() { string.Format("Imported document {0} into container {1}", documentName, context.OpenContainer.Name) };
             returnList.AddRange(context.OpenContainer.Documents);
             return returnList;
